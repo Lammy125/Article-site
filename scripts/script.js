4b974b80b1df99338129afea433dd493ce87d559
Adeyemo Peter <45455927+Lammy125@users.noreply.github.com>
2022-11-23 16:35:44 +0100

    Article Site

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+const baseUrl = "https://61924d4daeab5c0017105f1a.mockapi.io/credo/v1";
+
+const getDailyNews = async () => {
+    try {
+        const response = await fetch(`${baseUrl}/news`);
+        const data = await response.json();
+        console.log(data);
+        displayNews(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const displayNews = (data) => {
+    data.map((item) => {
+        let articleContainer = document.getElementById("articleContainer");
+
+        let article = document.createElement("div");
+        let articleImage = document.createElement("img");
+        let title = document.createElement("h4");
+        let link = document.createElement("a");
+
+        title.innerText = item.title;
+
+        articleImage.src = item.url;
+        articleImage.alt = "article image";
+        articleImage.width = "380";
+
+        articleImage.onerror = () => {
+            articleImage.onerror = null;
+            articleImage.src = "../assets/images/pexels-pixabay-531880 (1).jpg"
+        }
+
+        link.href = `/pages/content?id=${item.id}`;
+        link.target = "_blank";
+        link.innerText = "Read more";
+
+        article.appendChild(articleImage);
+        article.appendChild(title);
+        article.appendChild(link);
+
+        articleContainer.appendChild(article);
+    })
+}
+window.onload = getDailyNews();
